3932337877bfbf65b7baaa2cae00155d1044ae90
Nikita-123-orange <mulukov9265@gmail.com>
2025-09-10 00:08:09 +0300

    all exercise in src

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import importlib
+import sys
+import os
 
 def main():
+    src_path = os.path.join(os.path.dirname(__file__), 'src')
+    if src_path not in sys.path:
+        sys.path.insert(0, src_path)
     """Основная функция запуска упражнений"""
     while True:
         # Меню выбора
